f05d982df63edbd0e9b3a7c4816c5dff07f59e2e
RANA NIIKUNJ <8891nikunjrana@gmail.com>
2026-01-01 19:12:44 +0530

    Add files via upload

---
+++ C-lang(classRoom)/For-loop/Q-3.c
@@ -0,0 +1,16 @@
+//(Q-3) wap to print 1 to N using for loop.
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int n,i=1;
+	printf("Enter n:");
+	scanf("%d",&n);
+	
+	for(i=1;i<=n;i++)
+	{
+		printf(" %d ",i);
+	}
+}
