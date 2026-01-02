78b6abb16f766b1a0a8ca06ba412e3b2ae1fd5ec
RANA NIIKUNJ <8891nikunjrana@gmail.com>
2026-01-02 13:32:32 +0530

    Add files via upload

---
+++ C-lang(classRoom)/India-Bix-MCQ/Q-1.c
@@ -0,0 +1,12 @@
+// (Q-1)
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 2, i;
+    for (i = 2; i <= 8; i *= 2)
+    {
+        printf(" 1/ %d ", +i);
+    }
+}
---
+++ C-lang(classRoom)/India-Bix-MCQ/Q-14.c
@@ -0,0 +1,17 @@
+// (Q-14) Look at this series: 201, 202, 204, 207, ... What number should come next?
+
+#include <stdio.h>
+
+void main()
+{
+    int a = 201;
+    int i;
+
+    printf("%d ", a);
+
+    for (i = 1; i <= 5; i++)
+    {
+        a += i;
+        printf(" %d ", a);
+    }
+}
