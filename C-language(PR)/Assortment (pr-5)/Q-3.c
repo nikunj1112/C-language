d9f0b7164d59b2c1440544d36c4bfde45122aba8
RANA NIIKUNJ <8891nikunjrana@gmail.com>
2026-01-16 14:23:24 +0530

    Add files via upload

---
+++ C-language(PR)/Assortment (pr-5)/Q-3.c
@@ -0,0 +1,34 @@
+
+int main()
+{
+	int r, c, i, j;
+	printf("enter a row:");
+	scanf("%d", &r);
+	printf("enter a col:");
+	scanf("%d", &c);
+	int a[r][c];
+	for (i = 0; i < r; i++)
+	{
+		for (j = 0; j < c; j++)
+		{
+			printf("enter a[%d][%d]", i, j);
+			scanf("%d", &a[i][j]);
+		}
+	}
+	for (i = 0; i < r; i++)
+	{
+		for (j = 0; j < c; j++)
+		{
+			printf("%d ", a[i][j]);
+		}
+	}
+	printf("\n");
+	for (i = 0; i < c; i++)
+	{
+		for (j = 0; j < r; j++)
+		{
+			printf("%d ", a[j][i]);
+		}
+		printf("\n");
+	}
+}
---
+++ C-language(PR)/Assortment (pr-5)/Q-4.c
@@ -0,0 +1,34 @@
+
+int main()
+{
+	int r, c, i, j, sum = 0;
+	printf("enter a row:");
+	scanf("%d", &r);
+	printf("enter a col:");
+	scanf("%d", &c);
+	int a[r][c];
+	for (i = 0; i < r; i++)
+	{
+		for (j = 0; j < c; j++)
+		{
+			printf("enter a[%d][%d]", i, j);
+			scanf("%d", &a[i][j]);
+		}
+	}
+	for (i = 0; i < r; i++)
+	{
+		for (j = 0; j < c; j++)
+		{
+			printf("%d ", a[i][j]);
+		}
+	}
+	printf("\n");
+	for (i = 0; i < r; i++)
+	{
+		for (j = 0; j < c; j++)
+		{
+			sum = sum + a[i][j];
+		}
+	}
+	printf("sum is %d", sum);
+}
